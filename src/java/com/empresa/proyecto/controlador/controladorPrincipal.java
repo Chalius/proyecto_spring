3c13feb4c49f07187479bf0a093a184fcce1cb68
Gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-09 23:55:02 -0500

    muestra iniciar sesión y cerrar sesion según estado de usuario

--- src/java/com/empresa/proyecto/controlador/controladorPrincipal.java
+++ src/java/com/empresa/proyecto/controlador/controladorPrincipal.java
@@ -1,6 +1,11 @@
 package com.empresa.proyecto.controlador;
 
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,6 +47,16 @@ public class controladorPrincipal {
         ModelAndView mvc = new ModelAndView();
         mvc.setViewName("error-login");
         return mvc;
+    }
+
+    // PUNTO 5.3 LAB15 DAW
+    @RequestMapping(value = "salir", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/index.htm";
     }
 
 }
--- web/WEB-INF/jsp/datos_de_usuario.jsp
+++ web/WEB-INF/jsp/datos_de_usuario.jsp
@@ -1,9 +1,5 @@
-<%-- 
-    Document   : datos_de_usuario
-    Created on : 09/12/2019, 10:52:01 PM
-    Author     : gonzalo
---%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -16,14 +12,18 @@
             <h1>Datos de usuario</h1>
             <h3>Aquí podrás ver tus datos.</h3>
             <a href="index.htm">Ir a página principal</a>
-            
-        </div>
-        
-        
-        
-        
-        
-        
-        
-    </body>
+
+            <sec:authorize access="!hasRole('ROLE_ANONYMOUS')">     
+                <a href="<c:url value="salir.htm" />" class="btn btn-danger">Cerrar Sesion</a>       
+            </sec:authorize>
+
+    </div>
+
+
+
+
+
+
+
+</body>
 </html>
